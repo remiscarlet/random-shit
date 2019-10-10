9b231cb1e4d42781c3dc8105321c2e38591af7d5
Yuto Takamoto <scarletjaeger@gmail.com>
2019-10-10 04:05:22 +0000

    Split file into flute_finder.py and craigslist.py

--- craigslist.py
+++ craigslist.py
@@ -1,75 +1,22 @@
 import requests
-import csv
-import os
 import re
-import smtplib
 import secrets.craigslist_secrets as secrets
 
-urls = {
-    "craigslist": [
-        "https://newyork.craigslist.org/search/sss?format=rss&query=flute&sort=rel",
-        "https://newjersey.craigslist.org/search/sss?format=rss&query=flute&sort=rel",
-    ],
+#import dateutil
+import datetime
+
+url_list = {
+    "https://newyork.craigslist.org/search/sss?format=rss&query=flute&sort=rel",
+    "https://newjersey.craigslist.org/search/sss?format=rss&query=flute&sort=rel",
+    "https://hartford.craigslist.org/search/sss?format=rss&query=flute&sort=rel",
+    "https://newlondon.craigslist.org/search/sss?format=rss&query=flute&sort=rel",
+    "https://newhaven.craigslist.org/search/sss?format=rss&query=flute&sort=rel",
+    "https://nwct.craigslist.org/search/sss?format=rss&query=flute&sort=rel",
 }
 
-DATABASE_FILE = "db.csv"
-if not os.path.isfile(DATABASE_FILE):
-    with open(DATABASE_FILE, "w") as f:
-        f.write("")
+def search():
+    FOUND_URLS = {}
 
-DATABASE = {}
-with open(DATABASE_FILE, "r") as f:
-    reader = csv.reader(f)
-    for line in reader:
-        host, url, post_date = line
-        if host not in DATABASE:
-            DATABASE[host] = {}
-
-        DATABASE[host][url] = post_date
-
-def WRITE_TO_DB(host, url, date_posted):
-    global DATABASE
-    if host not in DATABASE:
-        DATABASE[host] = {}
-
-    DATABASE[host][url] = date_posted
-
-def SAVE_DB():
-    global DATABASE
-    global DATABASE_FILE
-    with open(DATABASE_FILE, "w") as f:
-        writer = csv.writer(f)
-        for host, data in DATABASE.items():
-            for url, date_posted in data.items():
-                writer.writerow([host, url, date_posted])
-
-def IS_NEW_TO_DB(host, url, date_posted):
-    global DATABASE
-    return host not in DATABASE or url not in DATABASE
-
-def NOTIFY_OF_NEW_URL(new_urls):
-    recipients = secrets.recipients
-
-    email_text = ""
-
-    for host, data in new_urls.items():
-        for url, date_posted in data:
-            email_text += "--\nTYPE: %s\nURL: %s\nDATE POSTED: %s\n\n" % (host, url, date_posted)
-
-    print email_text
-
-    sender = secrets.gmail_user
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(sender, secrets.gmail_password)
-
-    for recipient in recipients:
-        server.sendmail(sender, recipient, email_text)
-
-    server.close()
-
-NEW_URLS = {}
-for host, url_list in urls.items():
     for url in url_list:
         r = requests.get(url)
 
@@ -77,19 +24,13 @@ for host, url_list in urls.items():
         craigslist_re = re.compile(CRAIGSLIST_URL_AND_POST_REGEX)
         matches = re.findall(CRAIGSLIST_URL_AND_POST_REGEX, r.text, flags=re.DOTALL)
 
-        print matches
-
         for url, date_posted in matches:
-            print url, date_posted
-            if IS_NEW_TO_DB(host, url, date_posted):
-                WRITE_TO_DB(host, url, date_posted)
-
-                if host not in NEW_URLS:
-                    NEW_URLS[host] = []
-                NEW_URLS[host].append((url, date_posted))
+            date = datetime.datetime.strptime(date_posted, "%Y-%m-%dT%H:%M:%S-04:00") #2019-10-09T15:07:02-04:00
+            if date > datetime.datetime(2019, 10, 7):
+                FOUND_URLS[url] = date_posted
             else:
-                print "Not a new link... "+url
-        SAVE_DB()
+                print "FOUND URL POSTED BEFORE MINDATE: %s" % url
 
-if len(NEW_URLS) > 0:
-    NOTIFY_OF_NEW_URL(NEW_URLS)
+    return FOUND_URLS
+
+
---
+++ flute_finder.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+
+import requests
+import tempfile
+import csv
+import os
+import sys
+import re
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import secrets.craigslist_secrets as secrets
+
+import craigslist
+
+
+os.chdir("/tmp/")
+
+DATABASE_FILE = "db.csv"
+if not os.path.isfile(DATABASE_FILE):
+    with open(DATABASE_FILE, "w") as f:
+        f.write("")
+
+DATABASE = {}
+with open(DATABASE_FILE, "r") as f:
+    reader = csv.reader(f)
+    for line in reader:
+        host, url, post_date = line
+        if host not in DATABASE:
+            DATABASE[host] = {}
+
+        DATABASE[host][url] = post_date
+
+def WRITE_TO_DB(host, url, date_posted):
+    global DATABASE
+    if host not in DATABASE:
+        DATABASE[host] = {}
+
+    DATABASE[host][url] = date_posted
+
+def SAVE_DB():
+    global DATABASE
+    global DATABASE_FILE
+    with open(DATABASE_FILE, "w") as f:
+        writer = csv.writer(f)
+        for host, data in DATABASE.items():
+            for url, date_posted in data.items():
+                writer.writerow([host, url, date_posted])
+
+def IS_NEW_TO_DB(host, url, date_posted):
+    global DATABASE
+    return host not in DATABASE or url not in DATABASE[host]
+
+def NOTIFY_OF_NEW_URL(new_urls, is_real_run = False):
+    if is_real_run:
+        recipients = secrets.recipients
+    else:
+        recipients = [
+            "scarletjaeger+test@gmail.com",
+        ]
+
+    sender = secrets.gmail_user
+
+    new_urls = [(url, date_posted) for url, date_posted in new_urls.items()]
+    new_urls.sort(key = lambda tup: tup[0])
+
+    email_text = ""
+    tmp_handle, tmp_path = tempfile.mkstemp(suffix=".csv")
+    with open(tmp_path, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["url", "date_posted"])
+
+        for url, date_posted in new_urls:
+            email_text += "--\nURL: %s\nDATE POSTED: %s\n\n" % (url, date_posted)
+            writer.writerow([url, date_posted])
+
+    body = MIMEText(email_text, "plain")
+
+    with open(tmp_path, "r") as f:
+        attachment = MIMEText(f.read(), _subtype="text") # Hardcode cuz only sending CSVs
+    attachment.add_header("Content-Disposition", "attachment", filename="new_flute_listings.csv")
+
+    msg = MIMEMultipart()
+    msg["From"] = sender
+    msg["Subject"] = "New Flute Listings"
+    msg.attach(body)
+    msg.attach(attachment)
+
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(sender, secrets.gmail_password)
+
+    for recipient in recipients:
+        msg["To"] = recipient
+        server.sendmail(sender, recipient, msg.as_string())
+
+    server.close()
+
+def PROCESS_URLS(host, found_urls):
+    NEW_URLS = {}
+
+    for url, date_posted in found_urls.items():
+        if IS_NEW_TO_DB(host, url, date_posted):
+            WRITE_TO_DB(host, url, date_posted)
+
+            NEW_URLS[url] = date_posted
+        else:
+            print "Not a new link... "+url
+
+    SAVE_DB()
+
+    return NEW_URLS
+
+######################
+## RUN
+
+is_real_run = True if len(sys.argv) > 1 and sys.argv[1] == "--real-run" else False
+
+NEW_URLS = PROCESS_URLS("craigslist", craigslist.search())
+
+if len(NEW_URLS) > 0:
+    NOTIFY_OF_NEW_URL(NEW_URLS, is_real_run)
+
+
+
