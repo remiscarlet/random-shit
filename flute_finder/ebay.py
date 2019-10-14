37c9abb9d963cbba279c64a15eb6744caad61db4
Yuto Takamoto <scarletjaeger@gmail.com>
2019-10-14 16:40:28 -0400

    Add an important comment.

--- flute_finder/ebay.py
+++ flute_finder/ebay.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import requests
 import re
 
@@ -31,6 +32,7 @@ def search():
         url = BASE_URL % query_string
         r = requests.get(url)
 
+        # t⃘͢his i͓̍s bad beca͙̟use you cannot͚̟ parse html wit̜̐h̞̯ reg̰̭ex for ht⃓︠m⃗͆l is not a r᷍e̿ġu̅l̼aͣr̕ l̶ä́n͊g̟úǎg̴e̵ͧ n͇ͩO̟͜ s͙⃟t⃗͠O̓͞P̮ͪ y⃔̐O͗́̎u᷂᷉̕ c᷂̋̅Aͬͤ͠N⃓̛̏t⃒͔͆ S̰ͨͯT̆⃞⃒ͫO̮̯᷂ͥP᷊︣ͦͦ T᷄ͤͧ̐H᷿͔ͧ︣ë́᷃̅̂ P̨͙͐᷆̿ơͧ᷉͗̚N̮̤᷁᷃᷀Ý̛̝̺͗ H͖̊̍͗︡Ë̷͉̯͚́͘ C̤̙̃⃗̅ͅO̧̘̼᷿⃐ͫM̛᷂͕͎̃ͭe⃒͚⃗︣͒̚ṩ⃒̣̖͈̜S̢̃ͫ̓⃐⃛̇
         EBAY_URL_AND_POST_REGEX = "<li class=\"s-item.*?<a href=\"(https:\/\/www.ebay.com\/itm\/[^\"]*?)\".*?s-item__listingDate.*?class=\"BOLD\">(.*?)<\/span>.*?<\/li>"
         matches = re.findall(EBAY_URL_AND_POST_REGEX, r.text, flags=re.DOTALL)
 
