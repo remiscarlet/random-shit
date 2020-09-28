a383ae6dd233d01451fb99851bf43ed2b93f259e
Yuto Takamoto <scarletjaeger@gmail.com>
2020-09-28 15:16:41 -0400

    Create lib.js

---
+++ cjs/lib.js
@@ -0,0 +1,12 @@
+// Lib file for js used in CJS chrome extension
+// (https://chrome.google.com/webstore/detail/custom-javascript-for-web/poakhlngfciodnhlhhgnaaelnpjljija?hl=en)
+
+// sleep time expects milliseconds
+function mySleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+// Usage!
+#mySleep(5000).then(() => {
+#    // Stuff here
+#});
